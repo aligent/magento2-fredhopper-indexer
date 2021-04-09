ab7aa6e7dda151ba6b5a0e37539d7a2498ab2577
Benno Lang <benno.lang@aligent.com.au>
2021-04-09 14:46:45 +0930

    Don't export categories above root category
    As they generate a ton warnings in FH

--- Model/Indexer/Data/CategoryFieldsProvider.php
+++ Model/Indexer/Data/CategoryFieldsProvider.php
@@ -1,6 +1,8 @@
 <?php
 namespace Aligent\FredhopperIndexer\Model\Indexer\Data;
 
+use Aligent\FredhopperIndexer\Model\Export\Data\Meta;
+
 class CategoryFieldsProvider implements \Magento\AdvancedSearch\Model\Adapter\DataMapper\AdditionalFieldsProviderInterface
 {
     /**
@@ -8,10 +10,34 @@ class CategoryFieldsProvider implements \Magento\AdvancedSearch\Model\Adapter\Da
      */
     protected $index;
 
+    /**
+     * @var \Magento\Catalog\Api\CategoryRepositoryInterface
+     */
+    protected $categoryRepository;
+
+    /**
+     * @var \Aligent\FredhopperIndexer\Helper\GeneralConfig
+     */
+    protected $config;
+
+    /**
+     * @var int
+     */
+    protected $rootCategoryId;
+
+    /**
+     * @var int[]
+     */
+    protected $ancestorCategories;
+
     public function __construct(
-        \Magento\AdvancedSearch\Model\ResourceModel\Index $index
+        \Magento\AdvancedSearch\Model\ResourceModel\Index $index,
+        \Magento\Catalog\Api\CategoryRepositoryInterface $categoryRepository,
+        \Aligent\FredhopperIndexer\Helper\GeneralConfig $config
     ) {
         $this->index = $index;
+        $this->categoryRepository = $categoryRepository;
+        $this->config = $config;
     }
 
     /**
@@ -19,12 +45,38 @@ class CategoryFieldsProvider implements \Magento\AdvancedSearch\Model\Adapter\Da
      */
     public function getFields(array $productIds, $storeId)
     {
+        if (!isset($this->ancestorCategories)) {
+            $this->rootCategoryId = $this->config->getRootCategoryId();
+            $this->ancestorCategories = [];
+            try {
+                $rootCategory = $this->categoryRepository->get($this->rootCategoryId);
+                $rootAncestors = explode('/', $rootCategory->getPath());
+                $this->ancestorCategories = array_filter($rootAncestors);
+            } catch (\Exception $ex) {
+                // Root category configured incorrectly?
+                ;
+            }
+        }
+
         $result = [];
         // gives array of form [product id][category_id] = position
         $productCategoryData = $this->index->getCategoryProductIndexData($storeId, $productIds);
         foreach ($productCategoryData as $productId => $categoryInfo) {
+            $inRootCategory = isset($categoryInfo[$this->rootCategoryId]);
+
+            // Remove unwanted categories (root category and ancestors)
+            // as they won't be in FH and so each would generate a warning
+            foreach ($this->ancestorCategories as $ancestorId) {
+                unset($categoryInfo[$ancestorId]);
+            }
+
             // only care about category ids, not positions
             $result[$productId]['categories'] = array_keys($categoryInfo);
+
+            // Re-add to root category using FH-specific ref
+            if ($inRootCategory) {
+                $result[$productId]['categories'][] = Meta::ROOT_CATEGORY_NAME;
+            }
         }
         return $result;
     }
