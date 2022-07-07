da22de44c64ac21a0dc831dd464f4cf85fd6c5dd
Lachlan Turner <lachlan.turner@aligent.com.au>
2022-07-07 16:43:50 +0930

    Move custom attribute configuration to a single class instead of splitting between Products and Meta

---
+++ Helper/CustomAttributeConfig.php
@@ -0,0 +1,50 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Aligent\FredhopperIndexer\Helper;
+
+use Magento\Framework\App\Helper\Context;
+use Magento\Framework\Locale\Resolver;
+use Magento\Store\Model\StoreManagerInterface;
+
+class CustomAttributeConfig extends GeneralConfig
+{
+
+    /** @var array */
+    private array $customAttributeData;
+    /** string[] */
+    private array $siteVariantCustomAttributes;
+
+    public function __construct(
+        Context $context,
+        Resolver $localeResolver,
+        StoreManagerInterface $storeManager,
+        array $customAttributeData
+    ) {
+        parent::__construct($context, $localeResolver, $storeManager);
+
+        $this->customAttributeData = $customAttributeData;
+    }
+
+    public function getCustomAttributeData(): array
+    {
+        return $this->customAttributeData;
+    }
+
+    /**
+     * @return array
+     */
+    public function getSiteVariantCustomAttributes(): array
+    {
+        if (!isset($this->siteVariantCustomAttributes)) {
+            $this->siteVariantCustomAttributes = [];
+            foreach ($this->customAttributeData as $attributeCode => $attributeData) {
+                if ($attributeData['is_site_variant']) {
+                    $this->siteVariantCustomAttributes[] = $attributeCode;
+                }
+            }
+        }
+        return $this->siteVariantCustomAttributes;
+    }
+}
