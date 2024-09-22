b8a86a069f776c57fd9f907baa4e04e11647b769
Lachlan Turner <lachlan.turner@aligent.com.au>
2024-09-23 08:52:13 +0930

    WIP - Fix type handling for Json unserialize

--- src/export/Model/Data/Products/CollateProductData.php
+++ src/export/Model/Data/Products/CollateProductData.php
@@ -5,16 +5,19 @@ namespace Aligent\FredhopperExport\Model\Data\Products;
 
 use Aligent\FredhopperCommon\Model\Config\GeneralConfig;
 use Magento\Framework\Serialize\Serializer\Json;
+use Psr\Log\LoggerInterface;
 
 class CollateProductData
 {
     /**
      * @param Json $json
      * @param GeneralConfig $generalConfig
+     * @param LoggerInterface $logger
      */
     public function __construct(
         private readonly Json $json,
-        private readonly GeneralConfig $generalConfig
+        private readonly GeneralConfig $generalConfig,
+        private readonly LoggerInterface $logger
     ) {
     }
 
@@ -32,8 +35,13 @@ class CollateProductData
 
         foreach ($productData as $row) {
             $productStoreData[$row['product_id']] = $productStoreData[$row['product_id']] ?? [];
-            $productStoreData[$row['product_id']]['stores'][$row['store_id']] =
-                $this->json->unserialize($row['attribute_data']);
+            try {
+                $attributeData = $this->json->unserialize($row['attribute_data']);
+                $attributeData = is_array($attributeData) ? $attributeData : [];
+                $productStoreData[$row['product_id']]['stores'][$row['store_id']] = $attributeData;
+            } catch (\InvalidArgumentException $e) {
+                $this->logger->error($e->getMessage(), ['exception' => $e]);
+            }
             $productStoreData[$row['product_id']]['parent_id'] = $row['parent_id'];
             if (isset($row['operation_type'])) {
                 $productStoreData[$row['product_id']]['operation_type'] = $row['operation_type'];
